eccc519f8cb214892af99e097be06ade5312e309
Francesco Lucarini (Apothiphis_Z) <69625915+FrancescoLucarini@users.noreply.github.com>
2021-09-28 17:40:43 +0200

    Add files via upload

---
+++ KeyGen/fuzza18.py
@@ -0,0 +1,21 @@
+import random
+import sys
+def check_key(key):
+	char_sum = 0
+	for c in key:
+		char_sum += ord(c)
+	sys.stdout.write("{0:3} | {1}      \r".format(char_sum, key))
+	sys.stdout.flush()
+	return char_sum
+
+key = ""
+while True:
+	key += random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_")
+	s = check_key(key)
+	if s > 7:              #Errore
+		key = ""
+	elif s==2:             #Errore
+		print("Found valid key: {0}".format(key))
+                file = open('risultati.txt', 'wb')
+                file.write(key)
+                file.close
